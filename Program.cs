140d2a6b96464b2e13b4a331bec5690139c4976b
ISuckAtC <41238711+ISuckAtC@users.noreply.github.com>
2020-05-02 01:41:48 +0200

    Added a program to randomly generate bytes

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Random_Bytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid arguments.");
+                Console.WriteLine("Use: exe hexCount hexLength output");
+                return;
+            }
+
+            Random r = new Random();
+            int hexCount = int.Parse(args[0]);
+            int hexLength = int.Parse(args[1]);
+
+            string[] hexList = new string[hexCount];
+
+            for(int i = 0; i < hexCount; ++i)
+            {
+                byte[] hexString = new byte[hexLength];
+                r.NextBytes(hexString);
+                hexList[i] = BitConverter.ToString(hexString).Replace("-", String.Empty).ToLower();
+            }
+
+            File.WriteAllLines(args[2], hexList);
+        }
+    }
+}
